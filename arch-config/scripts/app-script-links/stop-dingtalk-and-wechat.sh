35376ea408e2e4faf19efe83588dc3502980b6e3
lemonwind <jomkiemail@gmail.com>
2023-11-25 23:36:09 +0800

    增加脚本

--- arch-config/scripts/app-script-links/stop-dingtalk-and-wechat.sh
+++ arch-config/scripts/app-script-links/stop-dingtalk-and-wechat.sh
@@ -3,15 +3,15 @@
 # Author: Jomkie
 
 unset dingtalkPid
-unset wechatPid
 dingtalkPid=$(ps -ef | grep -iE 'dingtalk' | grep -i '/com\.alibabainc\.dingtalk$' | grep -v 'grep' | awk '{print $2}')
 if [ -n $dingtalkPid ];then
 	kill -9 $dingtalkPid
 fi
+unset dingtalkPid
+
+#unset wechatPid
 #wechatPid=$(ps -ef | grep -iE 'wechat' | grep -i '/usr/lib/electron/electron --enable-crashpad /usr/lib/wechat-uos' | grep -v 'grep ' | awk '{print $2}')
 #if [ -n $wechatPid ];then
 #	kill -9 $wechatPid
 #fi
-
-unset dingtalkPid
-unset wechatPid
+#unset wechatPid
